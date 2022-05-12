b796b864710d630ece3ba1a6ef137539180335b1
Nur <olimovnurullo2001@gmail.com>
2022-05-12 10:19:25 +0500

    added searchview in CategoryFragment.kt

--- app/src/main/java/com/example/drinkapp/presentation/ui/CategoryFragment.kt
+++ app/src/main/java/com/example/drinkapp/presentation/ui/CategoryFragment.kt
@@ -2,40 +2,37 @@ package com.example.drinkapp.presentation.ui
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ProgressBar
-import android.widget.Toast
+import android.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.example.drinkapp.R
 import com.example.drinkapp.domain.model.CategoriesModel
 import com.example.drinkapp.domain.model.Drink
-import com.example.drinkapp.domain.model.DrinkDetails
 import com.example.drinkapp.presentation.adapter.DrinkAdapter
-import com.example.drinkapp.presentation.vm.CategoriesVm
 import com.example.drinkapp.presentation.adapter.ViewPagerAdapter
+import com.example.drinkapp.presentation.vm.CategoriesVm
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class CategoryFragment : Fragment() {
 
-    lateinit var ctgrArary: CategoriesModel
-    lateinit var adapter: ViewPagerAdapter
+    private lateinit var ctgrArary: CategoriesModel
+    private lateinit var adapter: ViewPagerAdapter
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var tabLayout: TabLayout
+    private lateinit var vm: CategoriesVm
+    private lateinit var progress: ProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_category, container, false)
-        val viewPager2: ViewPager2 = view.findViewById(R.id.viewpager)
-        val tabLayout = view.findViewById<TabLayout>(R.id.tab_layout)
-        val vm = ViewModelProvider(this).get(CategoriesVm::class.java)
-        val progress = view.findViewById<ProgressBar>(R.id.progress)
+        initViews(view)
 
         val click = object : DrinkAdapter.DrinkOnclick {
             override fun clickItem(id: Int) {
@@ -84,9 +81,39 @@ class CategoryFragment : Fragment() {
         })
 
         tabLayout.selectTab(tabLayout.getTabAt(5))
-
+        setHasOptionsMenu(true)
         return view
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.toolbar_menu, menu)
+
+        val mSearch = menu.findItem(R.id.action_search)
+        val mSearchView: SearchView = mSearch.actionView as SearchView
+        mSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Log.e("onQueryTextSubmit", "onQueryTextSubmit: doooooo")
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                Log.e("onQueryTextSubmit", "onQueryTextSubmit: $newText")
+                if (newText.isNotEmpty()) {
+                    tabLayout.visibility = View.GONE
+                } else{
+                    tabLayout.visibility = View.VISIBLE
+                }
+                return false
+            }
+        })
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+   private fun initViews(view: View) {
+        viewPager2 = view.findViewById(R.id.viewpager)
+        tabLayout = view.findViewById(R.id.tab_layout)
+        vm = ViewModelProvider(this).get(CategoriesVm::class.java)
+        progress = view.findViewById(R.id.progress)
+    }
 
 }
